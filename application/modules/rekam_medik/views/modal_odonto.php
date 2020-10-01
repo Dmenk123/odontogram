a4a3320bab0fd616cdc3506cd8951702a0347017
nurchyn <nurcahyono320@gmail.com>
2020-10-01 16:11:09 +0700

    odonto

--- application/modules/rekam_medik/views/modal_odonto.php
+++ application/modules/rekam_medik/views/modal_odonto.php
@@ -1,5 +1,10 @@
+<style>
+  .odonto-modal {
+    max-width: 90%;
+  }
+</style>
 <div class="modal fade modal_detail" tabindex="-1" role="dialog" aria-labelledby="add_menu" aria-hidden="true" id="div_odonto_modal">
-  <div class="modal-dialog modal-xl" role="document">
+  <div class="modal-dialog modal-lg odonto-modal" role="document">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="div_odonto_modal_title">Odontogram</h5>
@@ -7,7 +12,169 @@
         </button>
       </div>
       <div class="modal-body">
-        Konten Disini
+                <div class="row" align="center" style="flex-wrap:wrap; justify-content:center;">
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" display="none" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="90">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                  <svg height="125" width="60">
+                    <polyline points="2,22 57,22 57,77 2,77 2,22" style="fill:white;stroke:black;stroke-width:3" />
+                    <polyline points="4,24 4,75 15,63 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="4,24 55,24 43,35 15,35 4,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,24 55,75 43,63 43,35 55,24" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="55,75 4,75 15,63 43,63 55,75" style="fill:none;stroke:black;stroke-width:1" />
+                    <polyline points="15,35 43,35 43,63 15,63 15,35" style="fill:none;stroke:black;stroke-width:1" /> -->
+                    <text x="21" y="123" font-family="Verdana" font-size="13" fill="blue" >21</text>
+                     <text x="17" y="15" font-family="Verdana" font-size="13" fill="blue" align="center">PRE</text>
+                  </svg>
+                </div>
+      
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
